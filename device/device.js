a2eb9630f20e12e2445995feb637a71a431e5b4a
santoshkrishnanr@gmail.com <santoshkrishnanr@gmail.com>
2020-06-30 18:03:45 +0200

    added logic , prints response in terminal

--- device/device.js
+++ device/device.js
@@ -3,15 +3,62 @@ const socket = io.connect("http://localhost:4200/", {
     reconnection: true
 });
 
+const getcurrWeight = () => {
+    const weight = Math.floor(Math.random() * 6) + 1;
+    console.log(weight)
+    return weight;
+};
+
+const avg = (arr) => arr.reduce((a, b) => a + b) / arr.length;
+
+const getRespone = (stableWeight) => {
+    let res;
+    const minLoad = 2.9;
+    const maxLoad = 3.9;
+    if (stableWeight < minLoad) {
+        res = "S_-" //underload
+    } else if (stableWeight > maxLoad) {
+        res = "S_+" //overerload
+    } else {
+        res = `S_S_${ stableWeight}_kg`
+    }
+    return res;
+}
+
+const shortPerReading = () => {
+    let reading = [];
+    return new Promise((resolve, reject) => {
+        const readweights = () => {
+            w = getcurrWeight();
+            reading.push(w);
+            if (reading.length < 10) {
+                setTimeout(() => {
+                    readweights()
+                }, 10)
+            }else{
+                resolve(reading)
+            }
+        }
+        readweights();
+        console.log('++++++++++ current weight fluctuations ++++++++++++++++');
+    })
+}
 
 socket.on('connect', () => {
-     console.log("Device online");
-
-    /*socket.on('driverEvent', () => {
-        console.log('Listening');
-        
-        //console.log("sending state info to driver....");
-        //socket.emit('deviceEvent', stateInfo);
-    }) */
+    console.log("Device online")
 });
 
+socket.on('driverEvent', (command) => {
+    console.log('Listening');
+
+    if (command === 's') {
+        console.log('command recognized');
+        shortPerReading().then(readings => {
+            stableWeight = avg(readings);
+            res = getRespone(stableWeight);
+            socket.emit('deviceEvent', res);
+        })
+    } else {
+        socket.emit('deviceEvent', "S_I"); // response - command not executable
+    }
+})
--- driver/driver.js
+++ driver/driver.js
@@ -1,14 +1,18 @@
 const io = require('socket.io').listen(4200);
 
+const sendCmd = (socket) => {
+    console.log('Sending Command: Send stable weight value');
+    socket.emit('driverEvent',"s");
+};
+
 io.on('connection', (socket) => {
     console.log("Driver online \n");
 
-    /* socket.on('deviceEvent', (data) => {
-        
+    setInterval(() => {
+        sendCmd(socket)
+    },3000)
+
+    socket.on('deviceEvent', (data) => { 
+        console.log("command response :"+ data +"\n")       
     });
-
-    setTimeout(() => {
-        socket.emit('driverEvent', {});
-    },5000/3); */
 });
-
